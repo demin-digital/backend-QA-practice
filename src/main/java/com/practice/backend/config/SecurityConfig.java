bd4e8e578e587d09cc02c8450695d17b05bb8c31
Maxim P <maximp.qa@gmail.com>
2024-03-22 20:58:48 +0300

    resource server created

--- src/main/java/com/practice/backend/config/SecurityConfig.java
+++ src/main/java/com/practice/backend/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.practice.backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,7 +16,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers(HttpMethod.POST, "/auth")
+                .hasAuthority("SCOPE_read")
+                .and()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://localhost:7777/oauth2/jwks").build();
     }
 }
 
